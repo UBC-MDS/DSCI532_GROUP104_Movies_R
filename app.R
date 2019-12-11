600aa718bf58af14eed974e1929556a7a5336b95
Fanli Zhou <flizhou@gmail.com>
2019-12-10 23:38:07 -0800

    Fixed bug in app2.R, changed layout and combined app2.R and app.R

--- app.R
+++ app.R
@@ -31,6 +31,16 @@ genreDropdown <- dccDropdown(
   searchable=TRUE
 )
 
+directorDropdown <- dccDropdown(
+  id="Director",
+  options = map(
+    levels(as.factor(movies_df$Director)), function(x){
+      list(label=x, value=x)
+    }),
+  value='M. Night Shyamalan',
+  multi= TRUE,
+  searchable=TRUE
+)
 
 #' 
 #' Finds the number of movies of the most productive directors in the selected genre.
@@ -44,6 +54,7 @@ genreDropdown <- dccDropdown(
 #'
 
 all_genres <- unique(movies_df$Major_Genre)
+all_directors <- unique(movies_df$Director)
 
 make_plot <- function(data = movies_df, genres=all_genres){
   data1 <- data %>% 
@@ -67,21 +78,41 @@ make_plot <- function(data = movies_df, genres=all_genres){
       figure=make_plot() # gets initial data using argument defaults
     )
 
+make_plot2 <- function(data = movies_df, directors=all_directors){
+    top_df <- data %>% 
+      filter(Director %in% directors)
+    
+    IMDB <- top_df %>%
+      ggplot(aes(x = Year, y = IMDB_Rating, colour = Director)) +
+      geom_line() +
+      geom_point() +
+      labs(y = "IMDB Rating (Scale 1-10)", title = "IMDB Rating by Director") +
+      expand_limits(y = 1)
+    
+    ggplotly(IMDB, width = 500, height = 1000)
+}
+    graph2 <- dccGraph(
+      id = 'IMDB-graph',
+      figure=make_plot2() # gets initial data using argument defaults
+    )
+    
 # Set up the app layout
 app$layout(
   htmlDiv(
   list(
     htmlH1("Welcome to the Directors Production Tracker App"),
     htmlH4("Explore different directors based on the number of movies they produce in a genre to find your director for your next movie."),
-    htmlH3("**Instructions**"),
-    htmlH4("**Hold shift and click on one or more bars** on the bar chart to choose directors to view movie ratings and profits."),
-    htmlH4("Hover over a point for more details"),
+    dccMarkdown("**Instructions**"),
+    dccMarkdown("**Hold shift and click on one or more bars** on the bar chart to choose directors to view movie ratings and profits."),
+    dccMarkdown("Hover over a point for more details"),
     #selection components go here
 #    htmlLabel("Select a genre"),
     genreDropdown,
+    directorDropdown,
 #    htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
     #end selection components
     graph,
+    graph2,
     htmlIframe(height=20, width=10, style=list(borderWidth = 0)) #space
  #   dccMarkdown("[Data Source](https://cran.r-project.org/web/packages/gapminder/README.html)")
   
@@ -96,6 +127,16 @@ app$callback(
   function(genre_value){
    #' Takes in the genre and director and calls make_plot to update the plot
     make_plot(movies_df, genre_value)
+    
+})
+
+app$callback(
+  output=list(id = 'IMDB-graph', property='figure'),
+  params=list(
+              input(id = 'Director', property='value')),
+  function(director_value){
+    #' Takes in the genre and director and calls make_plot to update the plot
+    make_plot2(movies_df, director_value)
     
 })
 
